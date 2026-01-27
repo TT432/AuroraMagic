6aa997e869ebafbfc890d1b64ccfe9c00d2437d6
HaooooZhang <haoooo.Zhang@outlook.com>
2026-01-27 16:06:56 +0800

    Add new attributes for magic system: mana efficiency, spell power, affinities, and resistances

--- src/main/java/net/teamaurorisla/auroramagic/registry/AMAttribute.java
+++ src/main/java/net/teamaurorisla/auroramagic/registry/AMAttribute.java
@@ -1,13 +1,7 @@
 package net.teamaurorisla.auroramagic.registry;
 
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -16,12 +10,132 @@ import net.teamaurorisla.auroramagic.AuroraMagic;
 public class AMAttribute {
     public static final DeferredRegister<Attribute> ATTRIBUTE = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, AuroraMagic.MODID);
 
+    //魔导效能
     public static final RegistryObject<Attribute> MANA_EFFICIENCY = ATTRIBUTE.register("mana_efficiency",
             () -> new RangedAttribute(
                     "attribute.auroramagic.mana_efficiency",
-                    0.0,
+                    1.0,
                     0.0,
                     1024.0
             ).setSyncable(true)
     );
+
+    //法术强度
+    public static final RegistryObject<Attribute> SPELL_POWER = ATTRIBUTE.register("spell_power",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.spell_power",
+                    1.0,
+                    0.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+
+    //流派亲和
+    public static final RegistryObject<Attribute> BLAZE_AFFINITY = ATTRIBUTE.register("blaze_affinity",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.blaze_affinity",
+                    0.0,
+                    -1024.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+    public static final RegistryObject<Attribute> FROST_AFFINITY = ATTRIBUTE.register("frost_affinity",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.frost_affinity",
+                    0.0,
+                    -1024.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+    public static final RegistryObject<Attribute> EARTH_AFFINITY = ATTRIBUTE.register("earth_affinity",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.earth_affinity",
+                    0.0,
+                    -1024.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+    public static final RegistryObject<Attribute> GALE_AFFINITY = ATTRIBUTE.register("gale_affinity",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.gale_affinity",
+                    0.0,
+                    -1024.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+    public static final RegistryObject<Attribute> LIGHTNING_AFFINITY = ATTRIBUTE.register("lightning_affinity",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.lightning_affinity",
+                    0.0,
+                    -1024.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+    public static final RegistryObject<Attribute> DIVINE_AFFINITY = ATTRIBUTE.register("divine_affinity",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.divine_affinity",
+                    0.0,
+                    -1024.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+
+    //法术抗性
+    public static final RegistryObject<Attribute> SPELL_RESISTANCE = ATTRIBUTE.register("spell_resistance",
+        () -> new RangedAttribute(
+                "attribute.auroramagic.spell_resistance",
+                0.0,
+                -1024.0,
+                1024.0
+        ).setSyncable(true)
+    );
+    public static final RegistryObject<Attribute> BLAZE_RESISTANCE = ATTRIBUTE.register("blaze_resistance",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.blaze_resistance",
+                    0.0,
+                    -1024.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+    public static final RegistryObject<Attribute> FROST_RESISTANCE = ATTRIBUTE.register("frost_resistance",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.frost_resistance",
+                    0.0,
+                    -1024.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+    public static final RegistryObject<Attribute> EARTH_RESISTANCE = ATTRIBUTE.register("earth_resistance",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.earth_resistance",
+                    0.0,
+                    -1024.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+    public static final RegistryObject<Attribute> GALE_RESISTANCE = ATTRIBUTE.register("gale_resistance",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.gale_resistance",
+                    0.0,
+                    -1024.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+    public static final RegistryObject<Attribute> LIGHTNING_RESISTANCE = ATTRIBUTE.register("lightning_resistance",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.lightning_resistance",
+                    0.0,
+                    -1024.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+    public static final RegistryObject<Attribute> DIVINE_RESISTANCE = ATTRIBUTE.register("divine_resistance",
+            () -> new RangedAttribute(
+                    "attribute.auroramagic.divine_resistance",
+                    0.0,
+                    -1024.0,
+                    1024.0
+            ).setSyncable(true)
+    );
+
 }
